91b4a9b4123ddf62cb3a64844fd0e60a4cb3099d
Charles Hughes <chughes87@gmail.com>
2018-10-10 22:08:43 -0700

    Adjust for linux

--- keyboards/atreus/keymaps/charles/keymap.c
+++ keyboards/atreus/keymaps/charles/keymap.c
@@ -15,20 +15,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QW] = LAYOUT( /* Qwerty */
 	 KC_Q,   KC_W,     KC_E,    KC_R,    KC_T,                       KC_Y,   KC_U,    KC_I, KC_O, KC_P,
          KC_A,   KC_S,     KC_D,    KC_F,    KC_G,                       KC_H,   KC_J,    KC_K, KC_L, KC_SCLN,
-         KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,    KC_F8,   KC_ESC,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,
-         KC_TAB, KC_LALT,  KC_LCTL, KC_LGUI, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC, KC_BSPC, KC_MINS, KC_QUOT, KC_ENT),
+         KC_Z,   KC_X,     KC_C,    KC_V,    KC_B,    KC_BTN3, KC_ESC,   KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,
+         KC_TAB, KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC, KC_BSPC, KC_MINS, KC_QUOT, KC_ENT),
 
   [_RS] = LAYOUT( /* [> RAISE <] */
         KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,      KC_8,     KC_9,     KC_0,
         KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_ENT,
-        KC_HOME, KC_END,   KC_F7,   KC_F5,   KC_F9,   KC_NO,   KC_ESC,  KC_F11,  KC_F12,    KC_EQUAL, KC_GT,    KC_BSLS,
-        KC_ESC,  KC_LALT,  KC_LCTL, KC_LGUI, KC_LSFT, MO(_RS), MO(_LW), KC_SPC,  KC_DEL,    KC_LGUI,  KC_GRAVE, KC_F8),
+        KC_HOME, KC_END,   KC_MPRV,   KC_MPLY, KC_MNXT,   KC_NO,   KC_ESC,  KC_VOLD,  KC_VOLU,    KC_EQUAL, KC_GT,    KC_BSLS,
+        KC_ESC,  KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW), KC_SPC,  KC_DEL,    KC_LGUI,  KC_GRAVE, KC_F8),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
          KC_EXLM,  KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN,  KC_RPRN,
          KC_LBRC,  KC_RBRC, KC_LCBR,  KC_RCBR, KC_NO,                    KC_LEFT, KC_DOWN, KC_UP,      KC_RIGHT, KC_ENT,
-         KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   TO(_QW), KC_ESC,  KC_F6,   KC_F7,   KC_F8,      KC_F9,    KC_F10,
-         KC_NO,    KC_LALT, KC_LCTL,  KC_LGUI, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC,  KC_BSPC, KC_PSCREEN, KC_SLCK,  KC_PAUSE)
+         KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   RESET, KC_ESC,  KC_F6,   KC_F7,   KC_F8,      KC_F9,    KC_F10,
+         KC_NO,    KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC,  KC_BSPC, KC_PSCREEN, KC_SLCK,  KC_PAUSE)
 
 };
 
