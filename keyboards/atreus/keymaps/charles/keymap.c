26efe6b80ee45e4c73de01659f492a1498514d85
Charles Hughes <chughes87@gmail.com>
2018-11-14 10:00:27 -0800

    Add layout file

--- keyboards/atreus/keymaps/charles/keymap.c
+++ keyboards/atreus/keymaps/charles/keymap.c
@@ -20,15 +20,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RS] = LAYOUT( /* [> RAISE <] */
         KC_1,    KC_2,     KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,      KC_8,     KC_9,     KC_0,
-        KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_ENT,
+        KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_ESC,
         KC_HOME, KC_END,   KC_MPRV,   KC_MPLY, KC_MNXT,   KC_NO,   KC_ESC,  KC_VOLD,  KC_VOLU,    KC_EQUAL, KC_GT,    KC_BSLS,
         KC_ESC,  KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW), KC_SPC,  KC_DEL,    KC_LGUI,  KC_GRAVE, KC_F8),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
          KC_EXLM,  KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR,    KC_LPRN,  KC_RPRN,
-         KC_LBRC,  KC_RBRC, KC_LCBR,  KC_RCBR, KC_NO,                    KC_LEFT, KC_DOWN, KC_UP,      KC_RIGHT, KC_ENT,
+         KC_LBRC,  KC_RBRC, KC_LCBR,  KC_RCBR, KC_NO,                    KC_LEFT, KC_DOWN, KC_UP,      KC_RIGHT, KC_ESC,
          KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,   RESET, KC_ESC,  KC_F6,   KC_F7,   KC_F8,      KC_F9,    KC_F10,
-         KC_NO,    KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC,  KC_BSPC, KC_PSCREEN, KC_SLCK,  KC_PAUSE)
+         KC_NO,    KC_LGUI,  KC_LALT, KC_LCTL, KC_LSFT, MO(_RS), MO(_LW),  KC_SPC,  KC_BSPC, KC_PSCREEN, KC_F11,  KC_F12)
 
 };
 
