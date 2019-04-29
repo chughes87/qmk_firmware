ec7c14e138fa31cb181a4d2fe3fe30857e4056c8
Charles Hughes <chughes87@gmail.com>
2019-04-29 13:45:40 -0700

    Try dactyl like layout

--- keyboards/atreus/keymaps/charles/keymap.c
+++ keyboards/atreus/keymaps/charles/keymap.c
@@ -18,20 +18,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     	 KC_Q,   KC_W,     KC_E,    KC_R,           KC_T,                       KC_Y,          KC_U,            KC_I,    KC_O, KC_P,
          KC_A,   KC_S,     KC_D,    KC_F,           KC_G,                       KC_H,          KC_J,            KC_K,    KC_L, KC_SCLN,
          KC_Z,   KC_X,     KC_C,    KC_V,           KC_B,    KC_BTN3, KC_ESC,   KC_N,          KC_M,            KC_COMM, KC_DOT, KC_SLSH,
-         KC_TAB, KC_MINS,  KC_LALT, LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, LCTL_T(KC_BSPC), MO(_LW), KC_QUOT, KC_ENT),
+         KC_LALT,KC_TAB, KC_MINS,   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, LCTL_T(KC_BSPC), KC_QUOT, KC_ENT, MO(_LW)),
 
   [_RS] = LAYOUT( /* [> RAISE <] */
         KC_1,    KC_2,        KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,      KC_8,     KC_9,     KC_0,
         KC_LBRC, KC_RBRC,     KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_PIPE,
         KC_HOME, KC_END,      KC_MPRV, KC_MPLY, KC_MNXT, RESET,   KC_ESC,  KC_VOLD, KC_VOLU,   KC_EQUAL, KC_GT,    KC_BSLS,
-        KC_ESC,  S(KC_MINS),  KC_LALT, KC_LGUI, KC_LSHIFT, MO(_RS), MO(_RS), KC_SPC,  KC_DEL,    MO(_LW),  KC_GRAVE, KC_F8),
+        KC_LALT, KC_TAB, s(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW)),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
          KC_EXLM,  KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,   KC_F12,
          KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
          KC_LBRC,  KC_RBRC, KC_LCBR,  KC_RCBR, KC_NO,   RESET,   KC_ESC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_ENT,
-         KC_ESC,  S(KC_MINS),  KC_LALT, KC_LGUI, SFT_T(KC_SPC), MO(_RS), MO(_RS), SFT_T(KC_SPC),  KC_DEL,    MO(_LW),  KC_GRAVE, KC_F8)
-
+        KC_LALT, KC_TAB, s(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW))
 };
 
 const uint16_t PROGMEM fn_actions[] = {
