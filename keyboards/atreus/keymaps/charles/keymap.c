82842e4898b707cdf643e69d8dba775a82ad29cd
Charles Hughes <chughes87@gmail.com>
2019-05-12 20:30:47 -0700

    next version

--- keyboards/atreus/keymaps/charles/keymap.c
+++ keyboards/atreus/keymaps/charles/keymap.c
@@ -24,13 +24,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,    KC_2,        KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,      KC_8,     KC_9,     KC_0,
         KC_LBRC, KC_RBRC,     KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_PIPE,
         KC_HOME, KC_END,      KC_MPRV, KC_MPLY, KC_MNXT, RESET,   KC_ESC,  KC_VOLD, KC_VOLU,   KC_EQUAL, KC_GT,    KC_BSLS,
-        KC_LALT, KC_TAB, s(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW)),
+        KC_LALT, KC_TAB, S(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW)),
 
   [_LW] = LAYOUT( /* [> LOWER <] */
          KC_EXLM,  KC_AT,   KC_HASH,  KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_F11,   KC_F12,
          KC_F1,    KC_F2,   KC_F3,    KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
          KC_LBRC,  KC_RBRC, KC_LCBR,  KC_RCBR, KC_NO,   RESET,   KC_ESC,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_ENT,
-        KC_LALT, KC_TAB, s(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW))
+        KC_LALT, KC_TAB, S(KC_MINS),   LGUI_T(KC_ESC), KC_LSHIFT, MO(_RS), MO(_RS),  KC_SPC, KC_DEL, KC_GRAVE, KC_ENT, MO(_LW))
 };
 
 const uint16_t PROGMEM fn_actions[] = {
