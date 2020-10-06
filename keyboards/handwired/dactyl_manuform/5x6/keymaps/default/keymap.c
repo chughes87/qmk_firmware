900bcb1f1d108658099026b7cc9477f6e0e6e9f3
Charles Hughes <charleshu@zillowgroup.com>
2020-10-06 11:35:09 -0700

    stash

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
                                        KC_TAB, KC_MINS,                     KC_QUOT, KC_ENT,
                                LGUI_T(KC_ESC), KC_LSHIFT,                   KC_SPC, LCTL_T(KC_BSPC),
-                                        KC_LALT, RAISE,                      RAISE, KC_GRV,
+                                        RAISE, KC_LALT,                     KC_GRV, RAISE,
                                         KC_QUOT, KC_EQUAL,                    KC_MINS, KC_F8
   ),
 
@@ -36,20 +36,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
                                        KC_TAB, KC_MINS,                     KC_QUOT, KC_ENT,
                                LGUI_T(KC_ESC), KC_LSHIFT,                   KC_SPC, LCTL_T(KC_BSPC),
-                                        KC_LALT, RAISE,                      RAISE, KC_GRV,
+                                        RAISE, KC_LALT,                     KC_GRV, RAISE, 
                                         LOWER, KC_EQUAL,                    KC_MINS, LOWER
 
   ),
 
   [_RAISE] = LAYOUT_5x6(
-       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 ,    KC_F8 ,   KC_F9 ,   KC_F10 ,KC_F11 ,
-       _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,      KC_8,     KC_9,     KC_0,    _______,
-       _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_F3,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_PIPE, _______,
+       KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 ,    KC_F8 ,   KC_F9,    KC_F10,  KC_F11 ,
+       _______, KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                       KC_CIRC, KC_AMPR,   KC_ASTR,  KC_LPRN,  KC_RPRN, _______,
+       _______, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_F5,                     KC_LEFT, KC_DOWN,   KC_UP,    KC_RIGHT, KC_PIPE, _______,
        _______, KC_HOME, KC_END,  KC_MPRV, KC_MPLY, KC_MNXT,                   KC_VOLD, KC_VOLU,   KC_EQUAL, KC_GT,    KC_BSLS, _______,
                                        KC_TAB, KC_MINS,                     KC_GRV, KC_ENT,
                                LGUI_T(KC_ESC), KC_LSHIFT,                   KC_SPC, KC_DEL,
-                                        RESET, RAISE,                      RAISE, RESET,
-                                        KC_QUOT, KC_EQUAL,                    KC_MINS, KC_F8
+                                        RAISE, RESET,                       RESET, RAISE,
+                                        KC_QUOT, KC_EQUAL,                  KC_MINS, KC_F8
   ),
 };
-
